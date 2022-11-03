c7e2aea1b4f115bb65436f9536d73c024ce28b71
Paskinho <v.paskanov@gmail.com>
2022-11-03 16:04:59 +0300

    Done ps-3

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,8 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import axios from "axios";
 
-// Hi guys! Let`s reinforce our session:
 
-// 1. Install AXIOS -it`s a library for HTTP requests. We  use it instead method FETCH.
-// https://axios-http.com/ru/docs/intro
-// yarn add axios
-
-// axios.get('https://jsonplaceholder.typicode.com/todos')
-//     .then((res) => {
-//         setTodos(res.data)
-//     })
-
-//2. Let`s relocate our method map, and wrap it in a new variable:
-//const mapTodos=todos.map(el => {...
-
-// return (
-//     <div className="App">
-//         <button onClick={onClickHandler}>CLEAN POSTS</button>
-//         <ul>
-//             {mapTodos}
-//         </ul>
-//     </div>
-// );
-
-// 3. Create new button to redisplay  our data
-
-// 4. We are having a problem. The code is duplicated (axios.get...). Let`s create a new function and use it where we need.
-//Good luck!
-
-
-
-type PropsType=
+type PropsType =
     {
         userId: number,
         id: number,
@@ -43,38 +14,66 @@ type PropsType=
 function App() {
     const [todos, setTodos] = useState<Array<PropsType>>([])
 
-    axios.get('https://jsonplaceholder.typicode.com/todos')
-    .then((res) => {
-        setTodos(res.data)
-   })
+    const axiosRequest=()=>{
+        axios.get('https://jsonplaceholder.typicode.com/todos')
+            .then((res) => {
+                setTodos(res.data)
+            })
+    }
 
-    useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/todos')
-            .then(response => response.json())
-            .then(json => setTodos(json))
-    },[])
+    useEffect(() => {
+        // fetch('https://jsonplaceholder.typicode.com/todos')
+        //     .then(response => response.json())
+        //     .then(json => setTodos(json))
+
+        // axios.get('https://jsonplaceholder.typicode.com/todos')
+        //     .then((res) => {
+        //         setTodos(res.data)
+        //     })
+
+        axiosRequest()
+    }, [])
+
+    const mapTodos=todos.map(el=>{
+        return (
+            <li>
+                <span>{el.id} - </span>
+                <span>{el.title}</span>
+                <span>{el.completed}</span>
+            </li>
+        )
+    })
 
     const onClickHandler = () => {
         setTodos([])
     }
 
+    const onClickHandlerForRedisplay=()=>{
+        // axios.get('https://jsonplaceholder.typicode.com/todos')
+        //     .then((res) => {
+        //         setTodos(res.data)
+        //     })
+
+        axiosRequest()
+    }
+
     return (
         <div className="App">
             <button onClick={onClickHandler}>CLEAN POSTS</button>
+            <button onClick={onClickHandlerForRedisplay}>REDISPLAY</button>
             <ul>
-                {todos.map(el => {
-                    return (
-                        <li>
-                            <span>{el.id} - </span>
-                            <span>{el.title}</span>
-                            <span>{el.completed}</span>
-                        </li>
-                    )
-                })}
+                {/*{todos.map(el => {*/}
+                {/*    return (*/}
+                {/*        <li>*/}
+                {/*            <span>{el.id} - </span>*/}
+                {/*            <span>{el.title}</span>*/}
+                {/*            <span>{el.completed}</span>*/}
+                {/*        </li>*/}
+                {/*    )*/}
+                {/*})}*/}
 
+                {mapTodos}
             </ul>
-
-
         </div>
     );
 }
